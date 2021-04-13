04fa07dc79bbb97f9cd38e03dc4f1803920b15d2
Ghost-Ista <freddy.pacheco.est@tecazuay.edu.ec>
2021-04-12 22:40:46 -0400

    si funka

---
+++ src/main/java/com/ista/tradizone/di/app/PruebaTradizoneApplication.java
@@ -0,0 +1,13 @@
+package com.ista.tradizone.di.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTradizoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTradizoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ista/tradizone/di/app/PruebaTradizoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ista.tradizone.di.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTradizoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
