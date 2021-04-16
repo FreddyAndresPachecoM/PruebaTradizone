b9909bf73918676313b933ac0b8c828fa5b185f4
Eduardo Mendieta <chrisseduu13mc@gmail.com>
2021-04-16 11:28:58 -0500

    creando el metodo para subir usuarios

---
+++ src/main/java/com/ista/tradizone/di/app/controller/RestauranteController.java
@@ -0,0 +1,12 @@
+package com.ista.tradizone.di.app.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app")
+@CrossOrigin("*")
+public class RestauranteController {
+    
+}
--- src/main/java/com/ista/tradizone/di/app/controller/UsuarioController.java
+++ src/main/java/com/ista/tradizone/di/app/controller/UsuarioController.java
@@ -5,15 +5,15 @@ import com.ista.tradizone.di.app.service.UsuarioService;
 import com.ista.tradizone.di.app.util.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/app")
+@CrossOrigin("*")
 public class UsuarioController {
     
     @Autowired
@@ -21,7 +21,7 @@ public class UsuarioController {
 
 
     @PostMapping("/usuarios")
-    public Response<Usuario> crearUsuario(@RequestBody Usuario usuario, @PathVariable MultipartFile avatarUsuario){
-        return usuarioService.crearUsuario(usuario, avatarUsuario);
+    public Response<Usuario> crearUsuario(@RequestBody Usuario usuario){
+        return usuarioService.crearUsuario(usuario);
     }
 }
