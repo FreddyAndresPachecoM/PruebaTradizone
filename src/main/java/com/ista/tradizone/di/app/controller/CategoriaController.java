561e6b667e1e3aa650197332b73fc35d2cafa1b5
Eduardo Mendieta <chrisseduu13mc@gmail.com>
2021-04-17 17:19:15 -0500

    agregando categorias

---
+++ src/main/java/com/ista/tradizone/di/app/controller/CategoriaController.java
@@ -0,0 +1,34 @@
+package com.ista.tradizone.di.app.controller;
+
+import com.ista.tradizone.di.app.model.Categoria;
+import com.ista.tradizone.di.app.service.CategoriaService;
+import com.ista.tradizone.di.app.util.Response;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app")
+@CrossOrigin("*")
+public class CategoriaController {
+    
+    @Autowired
+    private CategoriaService categoriaService;
+
+
+    @PostMapping("/categorias")
+    public Response<Categoria> crearCategoria(@RequestBody Categoria categoria){
+       return categoriaService.crearCategoria(categoria);
+    }
+
+
+    @GetMapping("/categorias")
+    public Response<Categoria> getAllCategorias(){
+        return categoriaService.getAllCategorias();
+    }
+}
--- src/main/java/com/ista/tradizone/di/app/service/CategoriaService.java
+++ src/main/java/com/ista/tradizone/di/app/service/CategoriaService.java
@@ -1,8 +1,26 @@
 package com.ista.tradizone.di.app.service;
 
+import com.ista.tradizone.di.app.model.Categoria;
+import com.ista.tradizone.di.app.repository.CategoriaRepository;
+import com.ista.tradizone.di.app.util.HttpStatus;
+import com.ista.tradizone.di.app.util.Response;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoriaService {
     
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+    
+
+    public Response<Categoria> crearCategoria(Categoria categoria){
+        return new Response<>(HttpStatus.CREATED, "recurso creado exitosamente!", categoriaRepository.insert(categoria));
+    }
+
+
+    public Response<Categoria> getAllCategorias(){
+        return new Response<>(HttpStatus.Ok, "Ok!", categoriaRepository.findAll());
+    }
 }
